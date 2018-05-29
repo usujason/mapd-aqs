9350e44b9889396f61b9b5e064d6ff40b2b6677f
jason <jthomp@gmail.com>
2018-05-29 17:54:04 -0600

    Update AQS API.py

--- AQS API.py
+++ AQS API.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 # Retreive configuration parameters
 import configparser
 
@@ -17,10 +11,6 @@ apiUser = config['DEFAULT']['apiUser']
 apiPassword = config['DEFAULT']['apiPassword']
 outputFormat = config['DEFAULT']['outputFormat']
 
-
-# In[2]:
-
-
 # Set -or override config defaults- required API parameters
 aqsClass = 'AQI POLLUTANTS'
 bDate = '20170101'
@@ -28,21 +18,11 @@ eDate = '20170131'
 stateCode = '49'
 countyCode = '035'
 
-
-# In[3]:
-
-
 # Make an API request
 import requests
 
 requestURL = apiURL + 'user=' + apiUser + '&pw=' + apiPassword + '&format=' + outputFormat + '&pc=' + aqsClass + '&bdate=' + bDate + '&edate=' + eDate + '&state=' + stateCode + '&county=' + countyCode
-
 apiResp = requests.get(requestURL)
-
-print (apiResp.content)
-
-
-# In[4]:
 
 
 # Convert raw request into a Pandas data frame
@@ -50,10 +30,5 @@ import pandas as pd
 import io
 
 aqs_df = pd.read_csv(io.StringIO(apiResp.content.decode('utf-8')))
-
-
-# In[8]:
-
-
 aqs_df.head(n=20)
 
